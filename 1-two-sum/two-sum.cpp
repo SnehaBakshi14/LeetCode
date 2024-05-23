9b19e3313b6877c638c0d15d5f0a230b195d7ea4
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-23 16:15:40 +0530

    Time: 18 ms (49.44%) | Memory: 16.4 MB (7.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,12 +4,14 @@ public:
     {
         int n = nums.size();
         unordered_map<int,int> map;
-        for(int i = 0;i<n;i++){
-            map[nums[i]] = i;
+        for(int i = 0;i<n;i++)
+        {
+            map[nums[i]] = i;//index ki mapping o_o
         }
 
         for(int i =0;i<n;i++){
-            if(map.find((target - nums[i])) != map.end() && i != map[(target-nums[i])]){
+            if(map[(target - nums[i])]  && i != map[(target-nums[i])])
+            {
                 return {i,map[(target-nums[i])]};
             }
         }
