15d0453d0ac3f18c8b0ddf62ea2ae36d2496547a
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-23 17:07:30 +0530

    Time: 2 ms (51.18%) | Memory: 7.1 MB (50.00%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        vector<int> tribo(38,0);
-        tribo[0] = 0;
-        tribo[1] = 1;
-        tribo[2] = 1;
+        // Optimal Appraoch
 
-        for(int i=3;i<=n;i++){
-            tribo[i] = tribo[i-1] + tribo[i-2] + tribo[i-3];
+        // vector<int> tribo(38,0);
+        // tribo[0] = 0;
+        // tribo[1] = 1;
+        // tribo[2] = 1;
+
+        // for(int i=3;i<=n;i++)
+        // {
+        //     tribo[i] = tribo[i-1] + tribo[i-2] + tribo[i-3];
+        // }
+        // return tribo[n];
+
+        // Approach 2
+
+        if(n==0){
+            return 0;
         }
-        return tribo[n];
+        if(n==1 || n==2){
+            return 1;
+        }
+        int first = 0,second = 1,third = 1;
+        int sum = 0;
+        for(int i=3;i<=n;i++){
+            sum = first+second+third;
+            first = second;
+            second = third;
+            third = sum;
+        }
+        return sum;
     }
 };
