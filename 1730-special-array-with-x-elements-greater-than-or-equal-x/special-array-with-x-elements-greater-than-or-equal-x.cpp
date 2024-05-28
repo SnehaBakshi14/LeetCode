5407258c29cf9abb176b73f5e9258103c8d3ab0d
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-29 01:07:39 +0530

    Time: 2 ms (51.55%) | Memory: 10.5 MB (12.40%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -19,31 +19,54 @@ public:
 
     // Approach 2 x pr bhi binary search lga do
 
-    int specialArray(vector<int>& nums)
-    {
-        sort(begin(nums), end(nums));
-        int n = nums.size();
+    // int specialArray(vector<int>& nums)
+    // {
+    //     sort(begin(nums), end(nums));
+    //     int n = nums.size();
 
-        int l =0,r=n;
-        while(l<=r)
+    //     int l =0,r=n;
+    //     while(l<=r)
+    //     {
+    //         int  midx = l+(r-l)/2;
+    //         int idx = lower_bound(begin(nums),end(nums),midx)-begin(nums);
+    //         //1st element >= midx
+    //         int count = n - idx;
+    //         if(count == midx)
+    //         {
+    //             return midx;
+    //         }
+    //         else if(count <= midx)
+    //         {
+    //             r = midx-1;
+    //         }
+    //         else
+    //         {
+    //             l = midx+1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    // Approach 3 prefix sum
+
+     int specialArray(vector<int>& nums)
+     {
+        int n = nums.size();
+        vector<int> freq(n+1);
+        for(int i =0;i<n;i++)
         {
-            int  midx = l+(r-l)/2;
-            int idx = lower_bound(begin(nums),end(nums),midx)-begin(nums);
-            //1st element >= midx
-            int count = n - idx;
-            if(count == midx)
-            {
-                return midx;
-            }
-            else if(count <= midx)
-            {
-                r = midx-1;
-            }
-            else
-            {
-                l = midx+1;
-            }
+            int num = nums[i];
+            freq[min(num,n)]++;
+        }
+        int cum_sum =0;
+        for(int x=n;x>=0;x--)//traverse right to left
+        {
+            cum_sum += freq[x];
+            if(cum_sum == x)
+            return x;
         }
         return -1;
-    }
+
+     }
 };
