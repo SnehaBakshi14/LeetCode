8a6d073c89d17765d65372ddf82b8576b0d7ce7f
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-29 20:25:47 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (79.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,101 +1,88 @@
 class Solution {
 public:
+// Approch 1 Normal Recursion
+    //  vector<string> result;
+
+    // bool isValid(string str)
+    // {
+    //     // For string to be valid overall sum should be zero
+    //     int c = 0;
+    //     for(char ch : str)
+    //     {
+    //         if(ch == '(')
+    //             c++;
+    //         else
+    //          c--;
+    //         if(c<0)
+    //         {
+    //             return false;
+    //         }
+    //     }
+    //     return c==0;
+
+    // }
+
+
+    // void solve(string&curr, int n)
+    // {
+    //     if(curr.length() == 2*n)
+    //     {
+    //         if(isValid(curr))
+    //         {
+    //             result.push_back(curr);
+    //         }
+    //          return;
+    //     }
+    //     curr.push_back('(');//do
+    //     solve(curr,n);//explore
+    //     curr.pop_back();//undo
+
+    //     curr.push_back(')');//do
+    //     solve(curr,n);//explore
+    //     curr.pop_back();//undo
+    // }
+
+    // vector<string> generateParenthesis(int n)
+    // {
+    //     string curr = "";
+
+    //     solve(curr,n);
+        
+    //     return result;
+    // }
+
+
+// Approach 2 Smart recursion
      vector<string> result;
 
-    bool isValid(string str)
-    {
-        // For string to be valid overall sum should be zero
-        int c = 0;
-        for(char ch : str)
-        {
-            if(ch == '(')
-                c++;
-            else
-             c--;
-            if(c<0)
-            {
-                return false;
-            }
-        }
-        return c==0;
-
-    }
-
-
-    void solve(string&curr, int n)
+    void solve(string&curr, int n , int open ,int close)
     {
         if(curr.length() == 2*n)
         {
-            if(isValid(curr))
-            {
-                result.push_back(curr);
-            }
-             return;
-        }
+                result.push_back(curr);  
+                return;
+        }  
+        if(open < n)
+        {
         curr.push_back('(');//do
-        solve(curr,n);//explore
+        solve(curr,n,open+1,close);//explore
         curr.pop_back();//undo
-
+        }
+        if(close < open)
+        {
         curr.push_back(')');//do
-        solve(curr,n);//explore
+        solve(curr,n,open,close+1);//explore
         curr.pop_back();//undo
-
-
+        }
     }
-
-    vector<string> generateParenthesis(int n)
+     vector<string> generateParenthesis(int n)
     {
         string curr = "";
-
-        solve(curr,n);
+        int open =0;int close=0;
+        solve(curr,n ,open,close);
         
         return result;
     }
+
 };
-
-
-
-// class Solution {
-// public:
-//     vector<string> result;
-
-//     bool isValid(string str) {
-//         int count = 0;
-
-//         for(char ch:str) {
-//             if(ch == '(')
-//                 count++;
-//             else
-//                 count--;
-//             if(count < 0)
-//                 return false;
-//         }
-//         return count==0;
-//     }
-
-//     void solve(string& curr, int n) {
-//         if(curr.length() == 2*n) {
-//             if(isValid(curr)) {
-//                 result.push_back(curr);
-//             }
-//             return;
-//         }
-
-//         curr.push_back('(');
-//         solve(curr, n);
-//         curr.pop_back();
-
-//         curr.push_back(')');
-//         solve(curr, n);
-//         curr.pop_back();
-//     }
-
-//     vector<string> generateParenthesis(int n) {
-//         string curr = "";
-
-//         solve(curr, n);
-
-//         return result;
-//     }
-// };
 
