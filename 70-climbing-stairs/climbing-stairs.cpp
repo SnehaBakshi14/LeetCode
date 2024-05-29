c91377e20075aacf30985ed5ccaa184ab91d3129
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-30 01:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (65.69%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    //Approach 1 Bruteforce TLE
+    // int solve(int n) 
+    // {
+    //     if(n<0)
+    //     return 0;
+
+    //     if(n==0)
+    //     return 1;
+
+    //     int one_step = solve(n-1);
+    //     int two_step = solve(n-2);
+
+    //     return one_step+two_step;
+    // }
+    // int climbStairs(int n) 
+    // {
+    //     return solve(n);
+    // }
+
+    //Approach 2 Memoization
+    int t[46];
+    int solve(int n)
+    {
+        if(n < 0)
+        return 0;
+
+        if(t[n] != -1)
+        return t[n];
+
+         if(n==0)// we found one way to climb stairs
+        return 1;
+
+        int one_step = solve(n-1);
+        int two_step = solve(n-2);
+
+        return t[n] = one_step+two_step;
+    }
+    int climbStairs(int n)
+    {
+        memset(t,-1,sizeof(t));
+        return solve(n);
+    }
+
+};
