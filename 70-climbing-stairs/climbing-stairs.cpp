c90375835876cdd334e6cec7c0af6f355a8fc845
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-05-30 16:06:36 +0530

    Time: 3 ms (29.35%) | Memory: 7.3 MB (50.19%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -20,27 +20,46 @@ public:
     // }
 
     //Approach 2 Memoization
-    int t[46];
-    int solve(int n)
-    {
-        if(n < 0)
-        return 0;
+    // int t[46];
+    // int solve(int n)
+    // {
+    //     if(n < 0)
+    //     return 0;
 
-        if(t[n] != -1)
-        return t[n];
+    //     if(t[n] != -1)
+    //     return t[n];
 
-         if(n==0)// we found one way to climb stairs
-        return 1;
+    //      if(n==0)// we found one way to climb stairs
+    //     return 1;
 
-        int one_step = solve(n-1);
-        int two_step = solve(n-2);
+    //     int one_step = solve(n-1);
+    //     int two_step = solve(n-2);
 
-        return t[n] = one_step+two_step;
-    }
+    //     return t[n] = one_step+two_step;
+    // }
+    // int climbStairs(int n)
+    // {
+    //     memset(t,-1,sizeof(t));
+    //     return solve(n);
+    // }
+
+
+    // Approach 3 Bottoms down
     int climbStairs(int n)
     {
-        memset(t,-1,sizeof(t));
-        return solve(n);
+        vector<int> t(n+1);
+         if(n==1 || n==2)
+        return n;
+
+        t[0]=0;
+        t[1]=1;
+        t[2]=2;
+
+        for(int i=3;i<=n;i++)
+        {
+            t[i] = t[i-1]+t[i-2];
+        }
+        return t[n];
     }
 
 };
