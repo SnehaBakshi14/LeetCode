93b6eb8a352809785f9331142672d3dd24872d17
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-06-17 17:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -3,13 +3,29 @@ public:
     typedef long long ll;
     bool judgeSquareSum(int c) 
     {
-        for(ll a =0; a*a<=c;a++)
+        //Approach 1
+        // for(ll a =0; a*a<=c;a++)
+        // {
+        //     ll b = sqrt(c-a*a);
+        //     if(b*b == c-a*a)
+        //     {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //Approach 2 two pointer
+        ll b = sqrt(c);
+        ll a =0;
+        while(a<=b)
         {
-            ll b = sqrt(c-a*a);
-            if(b*b == c-a*a)
-            {
-                return true;
-            }
+            ll ans = a*a+b*b;
+            if(ans == c)
+            return true;
+            else if(ans<c)
+            a++;
+            else
+            b--;
         }
         return false;
     }
