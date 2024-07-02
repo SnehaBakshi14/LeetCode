72a5a7d44a13e2f646b47cd7ff80953423a33482
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-02 17:43:31 +0530

    Time: 7 ms (27.72%) | Memory: 12.7 MB (53.60%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -2,21 +2,49 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2)
     {
-        unordered_map<int,int>mp;
-        for(int&num : nums1)
+        // Approach 1 map 
+            // unordered_map<int,int>mp;
+            // for(int&num : nums1)
+            // {
+            //     mp[num]++;
+            // }
+            // vector<int>result;
+            // for(int i =0;i<nums2.size();i++)
+            // {
+            //     int n = nums2[i];
+            //     if(mp[n] > 0)
+            //     {
+            //         mp[n]--;
+            //         result.push_back(n);
+            //     }
+            // }
+            // return result;
+
+
+        // Approach 2
+        sort(begin(nums1),end(nums1));
+        sort(begin(nums2),end(nums2));
+        int i =0;//nums1
+        int j =0;//nums2
+        vector<int>res;
+        while( i < nums1.size() && j < nums2.size())
         {
-            mp[num]++;
-        }
-        vector<int>result;
-        for(int i =0;i<nums2.size();i++)
-        {
-            int n = nums2[i];
-            if(mp[n] > 0)
+            if(nums1[i] == nums2[j])
             {
-                mp[n]--;
-                result.push_back(n);
+                res.push_back(nums1[i]);
+                i++;
+                j++;
             }
+            else if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+            
         }
-        return result;
+        return res;
     }
 };
