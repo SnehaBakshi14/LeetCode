1609e887047872206c4950ba0806839a09c34578
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-07 01:12:49 +0530

    Time: 2 ms (54.30%) | Memory: 7.1 MB (31.20%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -3,21 +3,35 @@ public:
     int passThePillow(int n, int time) 
     {
         //Approach 1 simulation
-        int idx = 1;
-        int dir = 1;// left to right = 1,right to left = -1
-        while(time>0)
+        // int idx = 1;
+        // int dir = 1;// left to right = 1,right to left = -1
+        // while(time>0)
+        // {
+        //     if(idx+dir >=1 && idx+dir<=n)
+        //     {
+        //         idx += dir;
+        //         time--;
+        //     }
+        //     else
+        //     {
+        //         dir *= -1;// compliments the sign when out of boundary
+        //     }
+        // }
+        // return idx;
+
+        //Approach 2
+        int fullround = time/(n-1);
+        int timeleft = time%(n-1);
+        if(fullround %2 == 0)
         {
-            if(idx+dir >=1 && idx+dir<=n)
-            {
-                idx += dir;
-                time--;
-            }
-            else
-            {
-                dir *= -1;// compliments the sign when out of boundary
-            }
+            // even
+            return timeleft+1;
         }
-        return idx;
+        else
+        {
+            // odd 
+            return (n - timeleft);// reverse se pass 
+        }
         
     }
 };
