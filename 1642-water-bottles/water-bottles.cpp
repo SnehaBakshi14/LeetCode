6a843c583059baad03ab251d7e9717f37aece30b
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-08 02:03:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (30.83%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) 
     {
-        // Approavh 1 Simulation
-        int consumed = 0;
-        while(numBottles >= numExchange)
-        {
-            consumed += numExchange;
-            numBottles -= numExchange;
+    //     // Approavh 1 Simulation
+    //     int consumed = 0;
+    //     while(numBottles >= numExchange)
+    //     {
+    //         consumed += numExchange;
+    //         numBottles -= numExchange;
 
-            numBottles += 1;// because of every set consumed we gain a full bottle
+    //         numBottles += 1;// because of every set consumed we gain a full bottle
 
-        }
-        return numBottles+consumed;
-    }
+    //     }
+    //     return numBottles+consumed;// can consume the reamining full bottles also
+
+
+    // Approach 2 TC = O(1)
+    return numBottles + (numBottles - 1)/(numExchange-1);
+     }
+
+    
 };
