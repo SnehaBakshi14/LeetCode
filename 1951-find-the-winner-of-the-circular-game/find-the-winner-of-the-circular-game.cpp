3138ddcffa533de166f4d6168547ea00a065e4e3
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-08 23:58:22 +0530

    Time: 11 ms (40.21%) | Memory: 25.7 MB (20.59%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -3,18 +3,36 @@ public:
     int findTheWinner(int n, int k) 
     {
         //Approach 1 simulation with array
-        vector<int>arr;
-        for(int i =1;i<=n;i++)
+        // vector<int>arr;
+        // for(int i =1;i<=n;i++)
+        // {
+        //     arr.push_back(i);// Putting elements in circle
+        // }
+        // int i =0;// game starts from 1st player who is sitting at 0th index in arr
+        // while(arr.size() > 1)//till 1 friend is left
+        // {
+        //     int idx = (i + k -1) % arr.size();
+        //     arr.erase(arr.begin() + idx);
+        //     i = idx;
+        // }
+        // return arr[0];
+
+
+        // Approach 2 Queue simulation
+        queue<int> q;
+        for(int i =1 ;i<=n;i++)
         {
-            arr.push_back(i);// Putting elements in circle
+            q.push(i);
         }
-        int i =0;// game starts from 1st player who is sitting at 0th index in arr
-        while(arr.size() > 1)//till 1 friend is left
+        while(q.size() > 1)
         {
-            int idx = (i + k -1) % arr.size();
-            arr.erase(arr.begin() + idx);
-            i = idx;
+            for(int count =1;count<=k-1;count++)
+            {
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
         }
-        return arr[0];
+        return q.front();
     }
 };
