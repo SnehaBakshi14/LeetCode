0b02482962f529f5b3eac5516718494d994c7e45
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-30 22:29:17 +0530

    Time: 105 ms (38.76%) | Memory: 38.3 MB (40.83%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -3,19 +3,44 @@ public:
     int minimumDeletions(string s)
     {
         // Approach 1 using stack
+        // int n = s.length();
+        // int count =0;
+        // stack<char>st;
+        // for(int i =0;i<n;i++)
+        // {
+        //     if(!st.empty() && s[i] == 'a' && st.top()=='b')
+        //     {
+        //         st.pop();
+        //         count++;
+        //     }
+        //     else
+        //     {
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return count;
+
+        // Approach 2 using bcount and acount
         int n = s.length();
-        int count =0;
-        stack<char>st;
+        vector<int>right_a(n,0);// to keep count of no of a in right of every index
+        
+        int acount =0;
+        for(int i =n-1;i>=0;i--)// right traversal
+        {
+            right_a[i] = acount;
+            if(s[i] == 'a')
+            {
+                acount++;
+            }
+        }
+        int count = INT_MAX;
+        int bcount =0;
         for(int i =0;i<n;i++)
         {
-            if(!st.empty() && s[i] == 'a' && st.top()=='b')
+            count = min(count , bcount+right_a[i]);
+            if(s[i] == 'b')
             {
-                st.pop();
-                count++;
-            }
-            else
-            {
-                st.push(s[i]);
+                bcount++;
             }
         }
         return count;
