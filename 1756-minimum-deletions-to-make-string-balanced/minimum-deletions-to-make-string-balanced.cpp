cc71c2ede61e72d95e0c00ee42a5dc167420f5ed
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-07-30 22:37:28 +0530

    Time: 100 ms (44.98%) | Memory: 23.5 MB (76.55%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -21,13 +21,35 @@ public:
         // return count;
 
         // Approach 2 using bcount and acount
-        int n = s.length();
-        vector<int>right_a(n,0);// to keep count of no of a in right of every index
+        // int n = s.length();
+        // vector<int>right_a(n,0);// to keep count of no of a in right of every index
         
+        // int acount =0;
+        // for(int i =n-1;i>=0;i--)// right traversal
+        // {
+        //     right_a[i] = acount;
+        //     if(s[i] == 'a')
+        //     {
+        //         acount++;
+        //     }
+        // }
+        // int count = INT_MAX;
+        // int bcount =0;
+        // for(int i =0;i<n;i++)
+        // {
+        //     count = min(count , bcount+right_a[i]);
+        //     if(s[i] == 'b')
+        //     {
+        //         bcount++;
+        //     }
+        // }
+        // return count;
+
+        // Approach 3
+         int n = s.length();
         int acount =0;
         for(int i =n-1;i>=0;i--)// right traversal
         {
-            right_a[i] = acount;
             if(s[i] == 'a')
             {
                 acount++;
@@ -37,12 +59,17 @@ public:
         int bcount =0;
         for(int i =0;i<n;i++)
         {
-            count = min(count , bcount+right_a[i]);
+            if(s[i] == 'a')
+            {
+                acount--;
+            }
+            count = min(count , bcount+acount);
             if(s[i] == 'b')
             {
                 bcount++;
             }
         }
         return count;
+
     }
 };
