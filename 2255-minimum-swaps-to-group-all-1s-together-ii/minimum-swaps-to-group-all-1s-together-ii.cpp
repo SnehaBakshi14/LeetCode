56bd95c71f497d1761a19f8de294bd0e675c5945
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-08-02 22:23:05 +0530

    Time: 72 ms (40.47%) | Memory: 90 MB (24.36%) - LeetSync

---
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int>temp(2*n);
+        for(int i =0;i<2*n;i++)
+        {
+            temp[i] = nums[i%n]; // for append in circular array
+        }
+        int totalones = accumulate(begin(nums),end(nums),0);
+        int i =0,j=0;
+        int currones =0 ,maxcount =0;
+        while(j<2*n)
+        {
+            if(temp[j] == 1)
+            {
+                currones +=1;
+            }
+            if(j-i+1 > totalones)
+            {
+                currones -= temp[i];
+                i++;
+            }
+            
+            maxcount = max(maxcount ,currones);
+            j++;
+        }
+        return totalones-maxcount;
+    }
+};
