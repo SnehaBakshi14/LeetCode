18ee8b7aee98ab881b55289c29a544534f9c7a11
SnehaBakshi14 <snehabakshi1405@gmail.com>
2024-08-02 22:24:22 +0530

    Time: 80 ms (16.50%) | Memory: 83.1 MB (57.56%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -2,24 +2,48 @@ class Solution {
 public:
     int minSwaps(vector<int>& nums) 
     {
-        int n = nums.size();
-        vector<int>temp(2*n);
-        for(int i =0;i<2*n;i++)
-        {
-            temp[i] = nums[i%n]; // for append in circular array
-        }
+        // int n = nums.size();
+        // vector<int>temp(2*n);
+        // for(int i =0;i<2*n;i++)
+        // {
+        //     temp[i] = nums[i%n]; // for append in circular array
+        // }
+        // int totalones = accumulate(begin(nums),end(nums),0);
+        // int i =0,j=0;
+        // int currones =0 ,maxcount =0;
+        // while(j<2*n)
+        // {
+        //     if(temp[j] == 1)
+        //     {
+        //         currones +=1;
+        //     }
+        //     if(j-i+1 > totalones)
+        //     {
+        //         currones -= temp[i];
+        //         i++;
+        //     }
+            
+        //     maxcount = max(maxcount ,currones);
+        //     j++;
+        // }
+        // return totalones-maxcount;
+
+
+        // MODIFIED SPACE COMPLEXITY
+
+         int n = nums.size();
         int totalones = accumulate(begin(nums),end(nums),0);
         int i =0,j=0;
         int currones =0 ,maxcount =0;
         while(j<2*n)
         {
-            if(temp[j] == 1)
+            if(nums[j%n] == 1)
             {
                 currones +=1;
             }
             if(j-i+1 > totalones)
             {
-                currones -= temp[i];
+                currones -= nums[i%n];
                 i++;
             }
             
