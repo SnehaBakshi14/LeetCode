d37a3e4c08a4ba365b14f2abc5a9fbb9f70502de
SnehaBakshi14 <snehabakshi1405@gmail.com>
2025-02-03 22:22:38 +0530

    Time: 1346 ms (3.68%) | Memory: 319.5 MB (5.20%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int hIndex(vector<int>& arr) 
+    {
+         vector<int> bucket(10000000,0);
+        int n = arr.size();
+        int total = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] > n){
+                bucket[n]++;
+            }
+            else{
+                bucket[arr[i]]++;
+            }
+        }
+        for(int i=n;i>=0;i--){
+            total += bucket[i];
+            if(total >= i){
+                return i;
+            }
+        }
+        return n;
+    }
+    
+};
