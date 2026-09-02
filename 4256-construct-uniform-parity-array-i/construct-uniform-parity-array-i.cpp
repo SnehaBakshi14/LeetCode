4f718edb0228158bbd5079f6610cf807ff4cd4c2
SnehaBakshi14 <snehabakshi1405@gmail.com>
2026-09-02 20:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (7.23%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) 
+    {
+        //If all numbers are even → choose nums2[i] = nums1[i].If there is at least one odd number:Keep the odd number(s) as odd.For every even number, subtract any odd number → even - odd = odd.
+        return true;
+    }
+};
